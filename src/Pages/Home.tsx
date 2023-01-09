17d0f4e923f0c0e60bcb0bd199aabd4dccab587a
Charlie Bellow <charliebftm@gmail.com>
2023-01-09 08:50:12 -0300

    add the footer

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Portfolio from '../components/Portfolio'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
 		<>
 			<div className="flex flex-col items-center text-center justify-center bg-bgDark gap-2 w-fit h-full py-6 ">
-				{/*<h1 className="text-text text-xl  lg:text-5xl font-bold  text-center">Charlie's Portfolio</h1>*/}
 				<Portfolio />
+				<Footer />
 			</div>
 		</>
 	);
