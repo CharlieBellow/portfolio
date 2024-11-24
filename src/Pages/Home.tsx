7c5c403ebb8e4b1f3086f252e86880713f84e572
charlie <charliebftm@gmail.com>
2024-11-24 19:43:11 -0300

    fix width of scrollutton

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,11 +4,12 @@ import Skills from '../components/Skills'
 import { FaArrowDown } from 'react-icons/fa'
 
 export default function Home() {
-const [scrollButton, setScrollButton] = useState(true)
+  const [scrollButton, setScrollButton] = useState(true)
+
   useEffect(() => {
     setTimeout(() => {
       setScrollButton(false)
-     }, 10000)
+     }, 9000)
   })
   return (
     <div className=' h-screen bg-gray-light  flex overflow-hidden'>
@@ -17,7 +18,7 @@ const [scrollButton, setScrollButton] = useState(true)
      
       <main className=' w-full overflow-y-auto '>
         <div className='h-1/2 w-1/4 fixed -bottom-1/3 hidden lg:flex lg:right-1'>
-        {scrollButton && ( <div  className='scrollButton w-12 h-fit bg-gray-light rounded-full p-3'><FaArrowDown size={26} /></div>) }
+        {scrollButton && ( <div  className='scrollButton w-auto h-fit bg-gray-light rounded-full p-3'><FaArrowDown size={26} /></div>) }
 
         </div>
         <Skills />
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,25 +1,25 @@
-import AboutMe from './AboutMe'
-import charlie from '../assets/charliee.png'
-import Projects from './Projects'
-import SoftSkills from './SoftSkills'
-import HardSkills from './HardSkills'
-import Tecnology from './Tecnology'
+import charlie from "../assets/charliee.png"
+import AboutMe from "./AboutMe"
+import HardSkills from "./HardSkills"
+import Projects from "./Projects"
+import SoftSkills from "./SoftSkills"
+import Tecnology from "./Tecnology"
 
 export default function Skills() {
   return (
-    <div className='w-auto flex justify-center flex-col gap-8 overflow-y-clip'>
+    <div className="w-auto flex justify-center flex-col gap-8 overflow-y-clip">
       <img
         src={charlie}
-        alt='Charlie Bellow perfil'
-        className='w-auto -top-12'
+        alt="Charlie Bellow perfil"
+        className="w-auto -top-12"
       />
-      <div className='py-8'>
+      <div className="py-8">
         <AboutMe />
         <Projects />
-        <SoftSkills/>
+        <SoftSkills />
         <HardSkills />
-        <Tecnology/>
+        <Tecnology />
       </div>
     </div>
-  )
+  );
 }
