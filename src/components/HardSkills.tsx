d28f1fb830397c30d6a1403f12af86844eaace2a
charlie <charliebftm@gmail.com>
2025-10-22 20:31:33 -0300

    fix style in content

--- src/components/HardSkills.tsx
+++ src/components/HardSkills.tsx
@@ -2,12 +2,12 @@ import  Title  from './Title'
 
 export default function HardSkills() {
   return (
-    <div className='flex flex-col gap-8 py-6'>
-      <Title title='Habilidades Técnicas' />
-      <div className='m-2 grid xl:grid-cols-2 lg:grid-cols-1 gap-4 text-base 2xl:text-3xl p-4'>
-          <li>Desenvolvimento de Sistemas Web</li>
-          <li>Análise de Requisitos de Sistemas Web</li>
-          <li>UI/UX Design (Prototipação)</li>
+    <div className="flex flex-col gap-8 py-6">
+      <Title title="Habilidades Técnicas" />
+      <div className="m-2 grid xl:grid-cols-2 lg:grid-cols-1 gap-12 text-base 2xl:text-3xl p-4 px-10">
+        <li>Desenvolvimento de Sistemas Web</li>
+        <li>Análise de Requisitos de Sistemas Web</li>
+        <li>UI/UX Design (Prototipação)</li>
       </div>
     </div>
   )
--- src/components/SoftSkills.tsx
+++ src/components/SoftSkills.tsx
@@ -4,7 +4,7 @@ export default function SoftSkills() {
   return (
     <div className='flex flex-col gap-8 py-6'>
       <Title title='Habilidades Comportamentais' />
-      <div className='m-2 grid xl:grid-cols-2 lg:grid-cols-1 gap-4 text-base 2xl:text-3xl p-4'>
+      <div className='m-2 grid xl:grid-cols-2 lg:grid-cols-1 gap-12 text-base 2xl:text-3xl p-4 px-10'>
           <li>Comunicação</li>
           <li>Proatividade</li>
           <li>Criatividade</li>
